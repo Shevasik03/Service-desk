40691e548fc61cc0ec2ea2fdc75beaf2f732aaf2
Shevasik03 <sanyashev03@gmail.com>
2025-02-09 17:43:55 +0200

    c

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/service-desk/',  // Додаємо базовий шлях для правильного розміщення файлів
+  // base: '/service-desk/',  // Додаємо базовий шлях для правильного розміщення файлів
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.ts', '.tsx']  // Додаємо підтримку TypeScript розширень
