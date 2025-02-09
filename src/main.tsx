952122f94caa948253f4dfef41e280819d0521cb
Shevasik03 <sanyashev03@gmail.com>
2025-02-09 16:57:14 +0200

    vite

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store } from './redux/store.tsx';
 import './assets/style/main.css'
 
 import { Layout } from './layout/layout.tsx';
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/service-desk/",
+  base: './',  // Додаємо базовий шлях для правильного розміщення файлів
   plugins: [react()],
-})
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']  // Додаємо підтримку TypeScript розширень
+  }
+});
